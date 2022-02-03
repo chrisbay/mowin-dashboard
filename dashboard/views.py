48dcdb0f3c75c24db3a49c7ed106a5326fa10b26
Chris Bay <chris@chrisbay.net>
2022-02-03 05:27:38 -0600

    refactors templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from . import models
 
 class HomeView(TemplateView):
-    template_name = "index.html"
+    template_name = "index.jinja"
 
 
 class DashboardView(PermissionRequiredMixin, TemplateView):
-    template_name = "dashboard.html"
+    template_name = "dashboard.jinja"
     permission_required = "dashboard.view_dashboard"
     login_url = 'request-access'
     raise_exception = False
 
 class RequestAccessView(TemplateView):
-    template_name = "request_access.html"
+    template_name = "request_access.jinja"
--- mowindashboard/settings.py
+++ mowindashboard/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
             os.path.join(BASE_DIR, 'templates'),
         ],
         'OPTIONS': {
-            'match_extension': '.html',
+            'match_extension': '.jinja',
             'app_dirname': 'templates',
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
@@ -147,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
