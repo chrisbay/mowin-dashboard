c26a2ad8b90d48b2252d3850e192172589d7f27b
Chris Bay <chris@chrisbay.net>
2022-02-14 05:48:13 -0600

    Refactors home page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
-    path('request-access', views.RequestAccessView.as_view(), name='request-access'),
     path('dashboard', views.DashboardView.as_view(), name='dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,8 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from . import models
 
 class HomeView(TemplateView):
     template_name = "index.jinja"
 
 
-class DashboardView(PermissionRequiredMixin, TemplateView):
+class DashboardView(TemplateView):
     template_name = "dashboard.jinja"
-    permission_required = "dashboard.view_dashboard"
-    login_url = 'request-access'
-    raise_exception = False
-
-class RequestAccessView(TemplateView):
-    template_name = "request_access.jinja"
