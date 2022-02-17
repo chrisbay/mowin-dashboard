0e0ebc50fa90ea5288a6ed05fce9ba233000d9bd
Chris Bay <chris@chrisbay.net>
2022-02-17 09:23:08 -0600

    Fixes local db config

--- mowindashboard/settings.py
+++ mowindashboard/settings.py
@@ -147,6 +147,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
@@ -182,5 +188,6 @@ LOGIN_EXEMPT_URLS = [
 AUTH_USER_MODEL = 'dashboard.DashboardUser'
 SOCIAL_AUTH_USER_MODEL = 'dashboard.DashboardUser'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+if ENV != 'DEVELOPMENT':
+    # Activate Django-Heroku.
+    django_heroku.settings(locals())
