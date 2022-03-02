987831e0ba3cf5d6df9400dc73a76de44c185720
Chris Bay <chris@chrisbay.net>
2022-03-02 14:13:05 -0600

    Adds sortable date joined column

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -4,10 +4,10 @@ from .models import DashboardUser
 import pytz
 
 class DashboardUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined_cst')
+    list_display = ('email', 'first_name', 'last_name', 'date_joined', 'date_joined_cst')
     list_filter = ('groups', 'date_joined')
 
-    @admin.display(description='Created')
+    @admin.display(description='Date Joined (CST)')
     def date_joined_cst(self, obj):
         return obj.date_joined.astimezone(pytz.timezone('US/Central')).strftime('%b %d, %Y')
 
