82b490afb811426de467e158a12bc258da5beccc
Chris Bay <chris@chrisbay.net>
2022-03-02 13:28:52 -0600

    Customizes dashboard user admin view

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import DashboardUser
 
-admin.site.register(DashboardUser, UserAdmin)
+class DashboardUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined')
+    list_filter = ('groups', 'date_joined')
+
+admin.site.register(DashboardUser, DashboardUserAdmin)
