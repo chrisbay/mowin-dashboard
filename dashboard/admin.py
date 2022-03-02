203c81d1caf57cf22c96447e05c87bfec33d33fe
Chris Bay <chris@chrisbay.net>
2022-03-02 13:57:04 -0600

    Improves user creation date view for admins

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import DashboardUser
+import pytz
 
 class DashboardUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined_cst')
     list_filter = ('groups', 'date_joined')
+
+    @admin.display(description='Created')
+    def date_joined_cst(self, obj):
+        return obj.date_joined.astimezone(pytz.timezone('US/Central')).strftime('%b %d, %Y')
 
 admin.site.register(DashboardUser, DashboardUserAdmin)
